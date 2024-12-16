adcc757ad62baa6e2bd712b1df2b78b619c79e4d
PRAGATI <99424466+PRAGATI-2002@users.noreply.github.com>
2024-12-16 12:08:15 +0530

    Add files via upload

---
+++ components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>© 2024 Employee Nexus. All rights reserved. Terms and Conditions apply.</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
